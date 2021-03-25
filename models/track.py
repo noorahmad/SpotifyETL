b67349256e42000f45b7db6f4567630d8f82d99e
Noor <noorahmad23@gmail.com>
2021-03-24 20:31:44 -0700

    adding model for track

---
+++ models/track.py
@@ -0,0 +1,13 @@
+class Track:
+    """
+        Model for songs with album, artist, song name, filename, filepath
+        File name is stored to extract song name and artist
+        File path is stored to potentially extract the album
+        Album can be used to verify song from Spotify Search API call
+    """
+    def __init__(song, artist, album, filename, filepath):
+        self.song = song,
+        self.artist = artist
+        self.album = album,
+        self.filename = filename
+        self.filepath = filepath
