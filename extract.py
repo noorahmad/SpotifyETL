ef57efcaee4f891743ca94c6d9c2fa4efab407b1
Noor <noorahmad23@gmail.com>
2021-03-24 21:31:00 -0700

    updates to reading files

--- extract.py
+++ extract.py
@@ -7,9 +7,9 @@ def read_all_files(filepath):
     try:
         for root, dirs, files, in os.walk(filepath):
             for file in files:
-                with open(os.path.join(root, file), "r") as auto:       
+                with open(os.path.join(root, file), "r") as auto:
                     file_list.append(auto.name.split('\\')[1:])
-        logger.info('Total songs read: ' + len(file_list))
+        logger.info('Total songs read: ' + str(len(file_list)))
         return file_list
     except Exception as ex:
-        logger.error('ERROR Reading files' + ex)
+        logger.error(ex)
