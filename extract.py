4242b89ccb8e0b175ce4b4b502c92ffa19b25819
Noor <noorahmad23@gmail.com>
2021-03-24 20:35:41 -0700

    moved comments for extract, adding todo

--- extract.py
+++ extract.py
@@ -1,7 +1,11 @@
 import os
-
-# read files of the given file path and return in an array
+"""
+    TODO: Add directory to the file array in case we need to grab album 
+          in order to verify correct song is returned from spotify
+          Can also be extended so that songs may be added to specific playlists
+"""
 def read_all_files(filepath):
+    "Reads files of the given file path and return in an array"
     file_list = []
     for root, dirs, files, in os.walk(filepath):
         for file in files:
