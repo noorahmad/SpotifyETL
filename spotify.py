5c4e47f5e94768a2e2bd93cfdb2cb5f3486cac65
Noor <noorahmad23@gmail.com>
2021-03-24 19:09:17 -0700

    syntax changes

--- spotify.py
+++ spotify.py
@@ -1,10 +1,10 @@
 import requests
 import json
 
-class spotify_settings:
+class spotify:
     def auth_url():
         return 'https://accounts.spotify.com/api/token'
-    def payload():
+    def grant_type():
         return 'grant_type=client_credentials'
     def headers():
         return {
@@ -13,10 +13,12 @@ class spotify_settings:
             'Authorization': 'Basic ' + read_auth_code()
         }
 
+# read authentication code from text file
 def read_auth_code():
     auth_code = open('auth_code.txt', 'r')
     return auth_code.read()
 
+# authenticate with spotify etl and return response
 def authenticate():
-    auth_response = requests.request("POST", spotify_settings.auth_url(), data=spotify_settings.payload(), headers=spotify_settings.headers())
+    auth_response = requests.request("POST", spotify.auth_url(), data=spotify.grant_type(), headers=spotify.headers())
     return json.loads(auth_response.text)
