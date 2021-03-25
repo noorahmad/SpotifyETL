ffbed9b214a79b3262c9f804e00edffb433c15fb
Noor <noorahmad23@gmail.com>
2021-03-24 20:32:14 -0700

    adding logging, this needs to be extended

--- logger.py
+++ logger.py
@@ -1,1 +1,9 @@
+import logging
+import inspect
 
+logger = logging.getLogger('root')
+FORMAT = "[%(asctime)s] %(filename)s:%(lineno)s - %(funcName)20s(): %(message)s"
+logging.basicConfig(filename=r"logs/log.txt",
+                    format=FORMAT,
+                    datefmt='%H:%M:%S')
+logger.setLevel(logging.DEBUG)
